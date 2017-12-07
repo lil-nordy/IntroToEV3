244e4b202eb8e25c18b0e9a4a2e32933792d6c1a
nordqunn <nordqunn@rose-hulman.edu>
2017-12-07 09:48:26 -0500

    Got the name working! This is neat.

--- nordqunn/m1_say_your_name.py
+++ nordqunn/m1_say_your_name.py
@@ -17,3 +17,14 @@ Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
 #   Feel free to run it, change it, and run it again with any message you like.
 #   This program is simple, but if you can do this, then you have the tools to do FAR more.
 # ------------------------------------------------------------------
+
+import ev3dev.ev3 as ev3
+
+def main():
+    """ Creates the LargeMotor objects and passes them to other functions. """
+    print("--------------------------------------------")
+    print("  Testing the Name demo")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Nate Nordquist is a fan of this robot").wait()
+
+main()
